7aa04bfc42a68c44f65e26f68e0befa8b322fffa
Sergei <xsergeix@ukr.net>
2020-05-05 18:16:37 +0300

    vanilla

---
+++ task1.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const play=document.getElementById('myVideo')
+const btn=document.getElementById('myBtn')
+
+ const onClick=(e)=>{
+     play.play()
+     btn.removeEventListener()
+ }
+ btn.addEventListener('click',onClick)
+ 
